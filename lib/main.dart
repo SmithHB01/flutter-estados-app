805c06fbeeb766eae67a3f6455ada96b9a0ce2ad
Smith Herrera <brayanshg@gmail.com>
2023-10-04 12:04:17 -0500

    Fin Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:estados_app/pages/pages.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:estados_app/bloc/usuario/usuario_cubit.dart';
+import 'package:estados_app/pages/pages.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,14 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'pagina1',
-      routes: {
-        'pagina1': ( _ ) => const Pagina1Page(),
-        'pagina2': ( _ ) => const Pagina2Page(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: ( _ ) => UsuarioCubit() )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'pagina1',
+        routes: {
+          'pagina1': ( _ ) => const Pagina1Page(),
+          'pagina2': ( _ ) => const Pagina2Page(),
+        },
+      ),
     );
   }
 }
