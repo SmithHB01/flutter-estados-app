038277013485858b14a1f9dbc5e0f1ee6840102a
Smith Herrera <brayanshg@gmail.com>
2023-10-05 11:10:58 -0500

    Fin GetX

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,4 +1,8 @@
+import 'package:estados_app/controllers/usuario_controller.dart';
+import 'package:estados_app/models/usuario.dart';
 import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
 
 
 class Pagina1Page extends StatelessWidget {
@@ -7,21 +11,58 @@ class Pagina1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+  final usuarioController = Get.put( UsuarioController() );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pagina 1'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // usuarioController.removerUsuario();
+            }, 
+            icon: const Icon( Icons.delete_outline_outlined)
+          )
+        ],
       ),
-      body: const InformacionUsuario(),
+      
+      body: Obx(
+        () => usuarioController.existeUsuario.value
+        ? InformacionUsuario(usuario: usuarioController.usuario.value,)
+        : const Noinfo()
+      ),
+
      floatingActionButton: FloatingActionButton(
-      onPressed: () =>  Navigator.pushNamed(context, 'pagina2'),
+      // onPressed: () =>  Navigator.pushNamed(context, 'pagina2'),
+      onPressed: () =>  Get.toNamed('pagina2'),
       child: const Icon( Icons.navigate_next_outlined),
      ),
    );
   }
 }
 
+class Noinfo extends StatelessWidget {
+  const Noinfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      child: Center(
+        child: Text('No hay usuario seleccionado'),
+      ),
+    );
+  }
+}
+
 class InformacionUsuario extends StatelessWidget {
-  const InformacionUsuario({super.key});
+
+  final Usuario usuario;
+
+  const InformacionUsuario({
+    super.key,
+    required this.usuario
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -29,23 +70,23 @@ class InformacionUsuario extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       padding: const EdgeInsets.all(20.0),
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('General', style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold) ),
+          const Text('General', style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold) ),
 
-          Divider(),
+          const Divider(),
 
-          ListTile( title: Text('Nombre:') ),
-          ListTile( title: Text('Edad:') ),
+          ListTile( title: Text('Nombre: ${ usuario.nombre}') ),
+          ListTile( title: Text('Edad: ${ usuario.edad}') ),
 
-          Text('Profesiones', style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold) ),
+          const Text('Profesiones', style: TextStyle( fontSize: 18, fontWeight: FontWeight.bold) ),
 
-          Divider(),
+          const Divider(),
           
-          ListTile( title: Text('Profesion 1') ),
-          ListTile( title: Text('Profesion 2') ),
-          ListTile( title: Text('Profesion 3') ),
+          ...usuario.profesiones.map((profesion) => ListTile(
+            title: Text( profesion ),
+          ))
         ],
       ),
     );
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:estados_app/models/usuario.dart';
+import 'package:estados_app/controllers/usuario_controller.dart';
 
 
 class Pagina2Page extends StatelessWidget {
@@ -7,6 +11,10 @@ class Pagina2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final usuarioController = Get.find<UsuarioController>();
+    final Usuario usuario;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pagina 2'),
@@ -18,7 +26,19 @@ class Pagina2Page extends StatelessWidget {
 
             MaterialButton(
               onPressed: () {
-              
+                usuarioController.cargarUsuario( Usuario( nombre: 'Smith', edad: 25 ) );
+                Get.snackbar(
+                  'Usuario establecido',
+                  ' Smith es el nombre del usuario',
+                  backgroundColor: Colors.white,
+                  boxShadows: [
+                    const BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 10,
+
+                    )
+                  ]
+                );
               },
               color: Colors.blue,
               child: const Text('Establecer usuario', style: TextStyle( color: Colors.white ) ),
@@ -26,7 +46,7 @@ class Pagina2Page extends StatelessWidget {
 
             MaterialButton(
               onPressed: () {
-              
+                usuarioController.cambiarEdad(20);
               },
               color: Colors.blue,
               child: const Text('Cambiar edad', style: TextStyle( color: Colors.white ) ),
@@ -34,10 +54,18 @@ class Pagina2Page extends StatelessWidget {
 
             MaterialButton(
               onPressed: () {
-              
+                // usuarioController.agregarProfesion('profesion #${ usuarioController.usuario.value.profesiones.length +1 }');
+                usuarioController.agregarProfesion('profesion #${ usuarioController.profesionesCount +1 }');
               },
               color: Colors.blue,
               child: const Text('Añadir profesion', style: TextStyle( color: Colors.white ) ),
+            ),
+
+            MaterialButton(
+              onPressed: () => Get.changeTheme( Get.isDarkMode ? ThemeData.light() : ThemeData.dark() ),
+                
+              color: Colors.blue,
+              child: const Text('Cambiar tema', style: TextStyle( color: Colors.white ) ),
             ),
           ],
         ),
